e82b7ce35dd0d6513c28be58c840d8b0b00f2aa1
BLemine <baillahiamine@gmail.com>
2020-08-17 16:32:08 +0100

    add-dashboard

---
+++ scenes/dashboard/SideBar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {View} from "react-native";
+
+export default (props)=>{
+    return (
+        <View style={{backgroundColor:"red",width:150,height:300}}>
+
+        </View>
+    )
+}
---
+++ scenes/dashboard/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {View,Text} from "react-native";
+import { Drawer } from 'native-base';
+import Sidebar from "./SideBar";
+export default  (props)=>{
+    return (
+        <View>
+            <Text>Dashboard</Text>
+        </View>
+    )
+}
